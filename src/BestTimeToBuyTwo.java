6ec4a3475ddc7cdc0f22f7239500edef3ed7ff1d
Murvyn <marvin.asamoah.123@gmail.com>
2025-03-30 19:52:02 +0000

    change

--- src/BestTimeToBuyTwo.java
+++ src/BestTimeToBuyTwo.java
@@ -7,7 +7,6 @@ public class BestTimeToBuyTwo {
 
     public static int maxProfit(int[] prices) {
         int profit = 0;
-
         for (int i = 1; i < prices.length; i++) {
             if(prices[i] > prices[i - 1]) {
                 profit += prices[i] - prices[i - 1];
