d499b30864b9491ad037e8baf0fe0de70a44d712
Murvyn <marvin.asamoah.123@gmail.com>
2025-04-03 21:19:14 +0000

    implement product except its self

---
+++ src/ProductExceptSelf.java
@@ -0,0 +1,23 @@
+
+import java.util.Arrays;
+
+public class ProductExceptSelf {
+    public static void main (String[] args) throws Exception {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] output = new int[nums.length];
+        output[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            output[i] = output[i - 1] * nums[i - 1];
+        }
+        int rightProduct = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            output[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+        return output;
+    }
+}
