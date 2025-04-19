86377d2f6a993a89e324b6412eab97d00bb16570
Murvyn <marvin.asamoah.123@gmail.com>
2025-04-19 23:57:14 +0000

    change

--- src/LengthOfLastWord.java
+++ src/LengthOfLastWord.java
@@ -4,7 +4,6 @@ public class LengthOfLastWord {
         int length = lengthOfLastWord(s);
         System.out.println(length); // Output: 5
     }
-
     public static int lengthOfLastWord(String s) {
         String[] words = s.trim().split(" ");
         return words[words.length - 1].length();
